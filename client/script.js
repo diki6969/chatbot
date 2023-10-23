aaf2bc6cc53b2dc1f6f900606864c20f3a8cca21
IkyyOfficial <gendonmenjeng@gmail.com>
2023-10-23 12:27:12 +0700

    update client/script.js

--- client/script.js
+++ client/script.js
@@ -87,7 +87,7 @@ const handleSubmit = async e => {
     const response = await fetch(
         "https://aemt.me/prompt/gpt?prompt=" +
             encodeURIComponent(
-                "Namamu adalah Ikyy, kamu diciptakan oleh IkyyOFC, yang lebih dikenal dengan nama Diki Pandu Winata. Tugasmu adalah menjawab pertanyaan apapun secara rinci dan detail."
+                "Namamu adalah Ikyy. Kamu diciptakan oleh IkyyOFC, yang lebih dikenal dengan nama Diki Pandu Winata. Tugasmu adalah menjawab pertanyaan apa pun secara rinci dan detail."
             ) +
             "&text=" +
             encodeURIComponent(data.get("prompt")),
