11c9c05af76f7c5446fc26dba69689a05b11daba
IkyyOfficial <gendonmenjeng@gmail.com>
2023-11-03 21:36:57 +0700

    update client/script.js

--- client/script.js
+++ client/script.js
@@ -30,7 +30,7 @@ function typeText(element, text) {
         } else {
             clearInterval(interval);
         }
-    }, 5);
+    }, 1);
 }
 
 // generate unique ID for each message div of bot
