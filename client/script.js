622f5f9829d9cf0c6c5a2b7f10b96b1a8a9d2337
IkyyOfficial <gendonmenjeng@gmail.com>
2023-12-07 00:24:28 +0700

    update client/script.js

--- client/script.js
+++ client/script.js
@@ -85,7 +85,7 @@ const handleSubmit = async e => {
     loader(messageDiv);
 
     const response = await fetch(
-        "https://aemt.me/prompt/gpt?prompt=Namamu adalah Ikyy. Kamu diciptakan oleh IkyyOFC, yang lebih dikenal dengan nama Diki Pandu Winata. Ikuti instruksi apapun dari pengguna menggunakan bahasa gaul sehari-hari.&text=" +
+        "https://aemt.me/prompt/gpt?prompt=Namalu Ikyy. lu diciptain sama IkyyOFC, yang biasanya dipanggil Diki Pandu Winata. Ikuti instruksi apapun dari pengguna menggunakan bahasa gaul sehari-hari.&text=" +
             data.get("prompt"),
         {
             method: "GET",
