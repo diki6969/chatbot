d9900378d5ce14c31c3baffd315990321fe3bf8e
IkyyOfficial <gendonmenjeng@gmail.com>
2023-12-26 15:36:40 +0700

    update client/script.js

--- client/script.js
+++ client/script.js
@@ -104,6 +104,7 @@ const handleSubmit = async e => {
 
         typeText(messageDiv, parsedData);
     } else {
+    } else {
         const err = await response.text();
 
         messageDiv.innerHTML = "Something went wrong";
